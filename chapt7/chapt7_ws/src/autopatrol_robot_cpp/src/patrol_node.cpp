3f8a9b2e9ab7fff38f00e9aee4fa289b738ca64b
wht <1053309726@qq.com>
2025-08-17 22:11:39 +0800

    添加巡航控制节点的基本框架代码

--- chapt7/chapt7_ws/src/autopatrol_robot_cpp/src/patrol_node.cpp
+++ chapt7/chapt7_ws/src/autopatrol_robot_cpp/src/patrol_node.cpp
@@ -1,13 +1,44 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform.hpp"
 #include <chrono>
+#include <vector>
+
+using PoseStamped = geometry_msgs::msg::PoseStamped;
+using Transform = geometry_msgs::msg::Transform;
 
 class PatrolNode : public rclcpp::Node{
 public:
     PatrolNode():Node("patrol_node"){
         RCLCPP_INFO(this->get_logger(), "这是自动巡航节点！");
+
+        this->declare_parameter<std::vector<double>>("initial_point", std::vector<double>{0.0, 0.0, 0.0});
+        this->declare_parameter<std::vector<double>>("target_points", std::vector<double>{0.0, 0.0, 0.0});
+        
+        this->initial_point = this->get_parameter("initial_point").as_double_array();
+        this->target_points = this->get_parameter("target_points").as_double_array();
+    
     }
 private:
+    std::vector<double> initial_point;
+    std::vector<double> target_points;
+
+    void initRobotPose(){
+
+    }
+    std::vector<std::vector<double>> getTargetPoints(){
+
+    }
+    PoseStamped getPoseByXYYaw(double x, double y, double yaw){
+
+    }
+    void navigateToPose(const PoseStamped &target_pose){
+
+    }
+    Transform getCurrentPose(){
+        
+    }
 
 };
 
