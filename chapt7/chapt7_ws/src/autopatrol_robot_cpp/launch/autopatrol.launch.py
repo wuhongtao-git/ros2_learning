996c6edaeadab632100cdc8d60223325f6839d79
wht <1053309726@qq.com>
2025-08-20 00:11:30 +0800

    添加了配置文件，并添加了launch文件，便于在启动节点的时候配置参数

---
+++ chapt7/chapt7_ws/src/autopatrol_robot_cpp/launch/autopatrol.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('autopatrol_robot_cpp'),
+        'config',
+        'patrol_config.yaml'
+    )
+    
+    patrol_node = Node(
+        package='autopatrol_robot_cpp',
+        executable='patrol_node',
+        name='patrol_node',
+        parameters=[config],
+        output='screen'
+    )
+    
+    return LaunchDescription([
+        patrol_node,
+    ])
